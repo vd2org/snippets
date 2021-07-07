370e8e00d59c4da8f856f93b004ec579183b5dec
Ivan <33198864+vd2org@users.noreply.github.com>
2021-07-07 12:12:21 +0300

    Update snowflake_id.py

--- python/snowflake_id.py
+++ python/snowflake_id.py
@@ -3,24 +3,34 @@
 
 
 from time import time
-from typing import Iterator
+from typing import Iterator, Optional
 
 MAX_INSTANCE = 0b1111111111
 MAX_SEQ = 0b111111111111
 
 
-def snowflake(instance: int, seq: int = 0) -> Iterator[int]:
-    assert instance <= MAX_INSTANCE, f"Instance value must not exceed {MAX_INSTANCE}!"
+def snowflake(instance: int, seq: int = 0) -> Iterator[Optional[int]]:
+    assert 0 <= instance <= MAX_INSTANCE, f"instance value must be positive and not exceed {MAX_INSTANCE}!"
+    assert 0 <= seq <= MAX_SEQ, f"seq value must be positive and not exceed {MAX_SEQ}!"
 
     instance = instance << 12
+    last_time = 0
 
     while True:
-        seq += 1
+        current_time = int(time() * 1000)
 
-        if seq > MAX_SEQ:
+        if last_time == current_time:
+            if seq == MAX_SEQ:
+                yield None
+                continue
+            seq += 1
+        elif last_time > current_time:
+            yield None
+            continue
+        else:
             seq = 0
 
-        yield int(time() * 1000) << 22 | instance | seq
+        yield (last_time := current_time) << 22 | instance | seq
 
 
 def get_milliseconds(value: int) -> int:
